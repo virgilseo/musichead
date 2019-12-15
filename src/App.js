74ff41ad2a3b372cf0f1f0bf1bac32c138ea6bcb
Virgil-Avram <avraam.virgil@yahoo.com>
2019-12-15 12:17:54 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer () {
+
+  return(
+    <div className='footer-container'>
+     <footer className='footer'>
+      <p className='footer-text'>© 2019 Virgil Avram</p>
+     </footer>
+    </div>
+  )
+}
+
+export default Footer
