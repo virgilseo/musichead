cd7254ba42dfa2934c0aa0f0aacf34abb85076c6
Virgil-Avram <avraam.virgil@yahoo.com>
2020-02-21 12:57:06 +0100

    Update query on user input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './Header.js';
 import Search from './Search';
 import Footer from './Footer.js';
 
 function App() {
+
+  //Set initial state of the query
+
+  const [query, setQuery] = useState('');
+
+  //Update query on user input
+
+  function updateQuery(userQuery)  {
+    setQuery(userQuery);
+  }
+
+  console.log(query);
+
   return (
     <div className="App">
       <Header />
-      <Search />
+      <Search updateQuery = {updateQuery} />
       <Footer />
     </div>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 
-function Search() {
+function Search(props) {
 
   return(
     <div className='search container'>
      <div className='input-wrapper'>
-       <input type='text' placeholder='Search for artist' className='search-input'/>
+       <input
+         type='text'
+         placeholder='Search for artist'
+         className='search-input'
+         onChange={(event) => {props.updateQuery(event.target.value)}}
+       />
        <button className='search-btn'>Search</button>
      </div>
     </div>
