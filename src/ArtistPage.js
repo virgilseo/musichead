181d3580a05512a1c694f5ed4ede64357ae5fbdf
Virgil-Avram <avraam.virgil@yahoo.com>
2020-02-26 14:17:31 +0100

    Fetch artist releases when component mounts

--- src/ArtistPage.js
+++ src/ArtistPage.js
@@ -8,6 +8,7 @@ function ArtistPage(props) {
 
   const [data, setData] = useState([]);
   const [error, setError] = useState(false);
+  const [releases, setReleases] = useState([]);
 
   //Fetch artist information from the discogs api
 
@@ -26,9 +27,27 @@ function ArtistPage(props) {
 
   }, [props.hits.id])
 
+// Fetch artist discography from discogs api
+
+  useEffect(() => {
+
+    fetch(`https://api.discogs.com//artists/${props.hits.id}/releases`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong ...');
+      }
+     })
+      .then(data => setReleases(data.releases)).then(error => setError(false))
+      .catch(error => setError(true))
+
+  }, [props.hits.id])
+
   console.log(data);
   console.log(error);
-  console.log(props.hits.id)
+  console.log(props.hits.id);
+  console.log(releases);
 
 
   return (
