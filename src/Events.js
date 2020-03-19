2b213a79c495bff2d8d5fb7705c563dab171920a
Virgil-Avram <avraam.virgil@yahoo.com>
2020-03-19 12:23:37 +0100

    Display event information only when available

--- src/Events.js
+++ src/Events.js
@@ -13,15 +13,27 @@ function Events(props) {
       <ul>
       {events.map(event => (
         <li key={event.id}>
-          <p>Name:{event.name}</p>
-          <p>Venue:{event._embedded.venues[0].name}</p>
-          <p>Address: {event._embedded.venues[0].address.line1}</p>
-          <p>City: {event._embedded.venues[0].city.name} </p>
+          {events.name && (
+            <p>Name:{event.name}</p>
+          )}
+          {event._embedded.venues[0].name && (
+            <p>Venue:{event._embedded.venues[0].name}</p>
+          )}
+          {event._embedded.venues[0].address && (
+            <p>Address: {event._embedded.venues[0].address.line1}</p>
+          )}
+          {event._embedded.venues[0].city && (
+            <p>City: {event._embedded.venues[0].city.name} </p>
+          )}
           {event._embedded.venues[0].state && (
             <p>State: {event._embedded.venues[0].state.name}</p>
           )}
-          <p>Date: {event.dates.start.localDate}</p>
-          <a href={event.url}>Get tickets</a>
+          {event.dates.start.localDate && (
+            <p>Date: {event.dates.start.localDate}</p>
+          )}
+          {event.url && (
+            <a href={event.url}>Get tickets</a>
+          )}
         </li>
       ))}
       </ul>
