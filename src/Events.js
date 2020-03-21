394849859909863e5cfca2d31b4cdc88162de31b
Virgil-Avram <avraam.virgil@yahoo.com>
2020-03-21 14:40:45 +0100

    Add event name

--- src/Events.js
+++ src/Events.js
@@ -13,23 +13,23 @@ function Events(props) {
       <ul>
       {events.map(event => (
         <li key={event.id}>
-          {events.name && (
-            <p>Name:{event.name}</p>
+          {event.name && (
+            <p>{event.name}</p>
           )}
           {event._embedded.venues[0].name && (
-            <p>Venue:{event._embedded.venues[0].name}</p>
+            <p>{event._embedded.venues[0].name}</p>
           )}
           {event._embedded.venues[0].address && (
-            <p>Address: {event._embedded.venues[0].address.line1}</p>
+            <p>{event._embedded.venues[0].address.line1}</p>
           )}
           {event._embedded.venues[0].city && (
-            <p>City: {event._embedded.venues[0].city.name} </p>
+            <p>{event._embedded.venues[0].city.name} </p>
           )}
           {event._embedded.venues[0].state && (
-            <p>State: {event._embedded.venues[0].state.name}</p>
+            <p>{event._embedded.venues[0].state.name}</p>
           )}
           {event.dates.start.localDate && (
-            <p>Date: {event.dates.start.localDate}</p>
+            <p>{event.dates.start.localDate}</p>
           )}
           {event.url && (
             <a href={event.url}>Get tickets</a>
